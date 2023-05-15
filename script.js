f280c02a6a9e9074895357912081bc2c58e25784
Win Win Maw <82701495+Sytreacy@users.noreply.github.com>
2023-05-16 04:18:30 +0630

    add: light & dark mode, download cv, toggle navbar remove: projects link

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
+const themes = document.getElementsByClassName('themes');
+
+for (const theme of themes) {
+    theme.addEventListener("click", () => {
+        if (theme.dataset.theme === 'light') {
+            themes[1].classList.remove('d-none');
+        }
+        if (theme.dataset.theme === 'dark') { 
+            themes[0].classList.remove('d-none');
+        }
+        theme.classList.toggle('d-none');
+        document.body.classList.toggle('dark');
+    })
+}
+
 let currentLocation = window.scrollY;
 window.addEventListener("scroll", () => {
     const backToTop = document.getElementsByClassName('back-to-top')[0];
+    const topNav = document.getElementsByTagName('nav')[0];
     if (window.scrollY > 16 || window.scrollY > currentLocation) {
             backToTop.classList.remove("d-none");
-        } else {
-            backToTop.classList.add("d-none");
-        }
+            topNav.classList.remove("d-none");
+    } else {
+        backToTop.classList.add("d-none");
+        topNav.classList.add("d-none");
+    }
     currentLocation = window.scrollY;
 })
 
@@ -27,10 +45,15 @@ for (r of radios)  {
             }
             let datasetName = "data-category="+id;
             let blogs = document.querySelectorAll('[' + datasetName + ']');
-            console.log(datasetName);
+            
             for (b of blogs) {
                 b.classList.remove("d-none");
             }
         }
     });
 }
+
+const dl_btn = document.querySelector('.download-resume');
+dl_btn.addEventListener('click', () => {
+   window.open('Win Win Maw.pdf', '_blank');
+});
